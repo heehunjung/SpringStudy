5ba2c565387203d16c66a6b1403c1cab5b5bd04b
heehun <slrkqk2@naver.com>
2024-03-07 15:48:50 +0900

    first

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,35 +5,32 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-@Service
+@Service @Transactional
 public class MemberService {
     private final MemberRepository memberRepository;
     @Autowired
     public MemberService(MemberRepository memberRepository){
         this.memberRepository=memberRepository;
     }
-
     public Long join(Member member){
         // 같은 이름이 있는 중복 회원 x
         validateDuplicateMember(member);
         memberRepository.save(member); //set id 가 포함되어 있으니깐
         return member.getId(); //get id로 해당 멤버의 id를 저장해줌
     }
-
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName()).ifPresent(m->{
              throw new IllegalStateException("이미 존재하는 회원입니다.");
          });
     }
-
     public List<Member> findMembers(){
         return memberRepository.findAll();
     }
-
     public Optional<Member> findOne(Long memberId){
         return memberRepository.findById(memberId);
     }
